8b6cdc3a87dfd7828ccfc53d312847da18734257
Ramu Kokare <ramkokare04@gmail.com>
2026-07-22 23:05:02 +0530

    Time: 9 ms (43.73%) | Memory: 42.1 MB (52.19%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,6 @@
 class Solution {
     public int fib(int n) {
-       return fibo(n);
-    }
-    static int fibo(int n){
-        if(n < 2){
-            return n;
-        }
-
-        return fibo(n - 1) + fibo(n - 2);
+        if (n < 2) return n;
+        return fib(n - 1) + fib(n - 2);
     }
 }
