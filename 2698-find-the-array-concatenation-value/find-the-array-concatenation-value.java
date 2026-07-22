c1cb1547029c926a542732bc7ee17cac6f7a60a8
Ramu Kokare <ramkokare04@gmail.com>
2026-07-22 09:13:22 +0530

    Time: 1 ms (90.94%) | Memory: 44.7 MB (83.44%) - LeetSync

---
+++ 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+       long sum = 0;
+       int l = 0;   
+       int r = nums.length - 1;
+       while (l < r) {
+        sum += concate (nums[l], nums[r]);
+        l++;
+        r--;
+       }
+       if (r == l) sum += nums[l];
+       return sum;
+    }
+    private static long concate(int a, int b) {
+        int len = digitLen(b);
+        double pow = Math.pow(10, len);
+        long c = (long) ((a * pow) + b);
+        return c;
+    }
+    private static int digitLen (int b) {
+        int len = 0;
+        while (b > 0) {
+            len++;
+            b /= 10;
+        }
+        return len;
+    }
+}
