966233d081c4028bba58ff6ae479cff279e8cdf0
Ramu Kokare <ramkokare04@gmail.com>
2026-08-13 22:30:17 +0530

    Time: 7 ms (30.57%) | Memory: 47.7 MB (94.62%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+       HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        } 
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (map.get(num) == 1) {
+                list.add(num);
+            }
+        }
+        int[] arr = new int[list.size()];
+        int i = 0;
+        for (int n : list) {
+            arr[i] = n;
+            i++;
+        }
+
+        return arr;
+    }
+}
